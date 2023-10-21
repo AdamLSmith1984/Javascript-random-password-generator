3fda9f8dddc632ade140c43898b6e03e9eb776b5
Adam Smith <asmith5144@hotmail.com>
2023-10-20 22:15:12 -0400

    added password prompts

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ const characterTypes = function () {
     passwordCriteria.lowercase = passwordCriteria.lowercase.toLowerCase();
     if (passwordCriteria.lowercase === "yes") {
         window.alert("You have chosen to include lowercase characters.");
-        passwordCriteria.lowercase = false;
+        passwordCriteria.lowercase = true;
     }
     else if (passwordCriteria.lowercase === "no") {
         window.alert("You have chosen not to include lowercase characters.");
@@ -63,8 +63,52 @@ const characterTypes = function () {
     }
 };
 
+// generates password through prompts
+const generatePassword = function () {
+    //prompt to set length of the password
+    passwordCriteria.length = window.prompt("Choose the length of the passwrd (at least 8 characters and no more than 128).");
+    // changes string from passwordLength prompt to an integer
+    passwordCriteria.length = parseInt(passwordCriteria.length);
+    // asks user to enter valid length
+    if (passwordCriteria.length < 8 || passwordCriteria.length > 128) {
+        window.alert("You did not enter a valid length. Please try again.");
+        generatePassword();
+    }
+    else {
+        window.alert("You have entered a length of " + passwordCriteria.length + ".");
+    }
 
+    window.alert("What character types do you want to use? Choose at least one.");
 
+    characterTypes();
+
+    window.alert("Generating password.");
+
+    function makePassword() {
+        var characters = "";
+        if (passwordCriteria.lowercase === true) {
+            characters = "abcdefghijklmnopqrstuvwxyz";
+        }
+        if (passwordCriteria.uppercase === true) {
+            characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        }
+        if (passwordCriteria.numeric === true) {
+            characters = "0123456789";
+        }
+        if (passwordCriteria.specialCharacters === true) {
+            characters = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+        }
+        var result = "";
+        var length = passwordCriteria.length;
+        for (var i = 0, n = characters.length; i < length; i++) {
+            result += characters.charAt(Math.floor(Math.random() * n));
+        }
+        window.alert("Your new password is" + result);
+        return result;
+    }
+
+    makePassword();
+};
 
 // Write password to the #password input
 function writePassword() {
